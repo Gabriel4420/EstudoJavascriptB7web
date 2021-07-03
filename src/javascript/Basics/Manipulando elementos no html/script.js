63d44ff5ea7ee4d82ec89e915834390fe0b20580
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2021-07-02 21:36:45 -0300

    manipulando classes

--- src/javascript/Basics/Manipulando elementos no html/script.js
+++ src/javascript/Basics/Manipulando elementos no html/script.js
@@ -1,3 +1,24 @@
-document.getElementById('titulo').innerHTML = "SARVE"
+/* document.getElementById('titulo').innerHTML = "SARVE"
 
 document.write("algum texto");
+ */
+const mudaCor = (cor) => {
+    if(cor == "verde"){
+        document.querySelector('#exemplo').classList.remove('vermelho');
+        document.querySelector('#exemplo').classList.remove('azul');
+        document.querySelector('#exemplo').classList.add('verde')
+    }
+    if(cor == "vermelho"){
+        document.querySelector('#exemplo').classList.remove('verde');
+        document.querySelector('#exemplo').classList.remove('azul');
+        document.querySelector('#exemplo').classList.add('vermelho')
+    }
+    if(cor == "azul"){
+        document.querySelector('#exemplo').classList.remove('vermelho');
+        document.querySelector('#exemplo').classList.remove('verde');
+        document.querySelector('#exemplo').classList.add('azul')
+    }
+       
+}
+
+mudaCor();
