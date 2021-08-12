9cba18c34c0a55e819e2e36e4b58502e99e71c83
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2021-08-11 23:29:53 -0300

    refactor: :art: adicionando um metodo alternativo ao createObjectURL

--- src/javascript/advanced/Thumbnails/index.js
+++ src/javascript/advanced/Thumbnails/index.js
@@ -6,3 +6,23 @@ function show() {
     document.querySelector('#area').appendChild(img);
 
 }
+/* 
+    Mostrando thumbnail com File Reader 
+
+    let reader = new FileReader();
+
+    let img = document.getElementById('img').files[0];
+
+    reader.onload =  () => {
+        let image = document.createElement('img');
+
+        img.src = reader.result;
+
+        img.width = 200;
+
+        document.querySelector('#area').appendChild(image);
+    }
+
+    reader.readAsDataURL(img);
+
+*/
